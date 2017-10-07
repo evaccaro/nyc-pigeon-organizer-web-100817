e3023c90a612b38b4fbf08bc8469c72c3b67e747
Elisa Vaccaro <erv230@nyu.edu>
2017-10-07 15:45:06 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |cat, subcat|
+    subcat.values.each do |names|
+      pigeon_list[names] = {}
+    end
+  end
+  pigeon_list
 end
