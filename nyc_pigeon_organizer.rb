20a3e2f3526a31713d8600d64ab2c4a5f127a551
Elisa Vaccaro <erv230@nyu.edu>
2017-10-07 16:09:26 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,7 +15,7 @@ def nyc_pigeon_organizer(data)
     pigeon_list[name][cat] = []
     subcat.each do |type, pigeons|
       if pigeons.include?(name)
-        pigeon_list[name][cat].push(type)
+        pigeon_list[name][cat].push(type.to_s)
       end
     end
   end
